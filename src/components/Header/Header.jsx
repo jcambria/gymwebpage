bcc5766222c7a5019e82204c73cf6b0f3aa04d2f
Justin  Cambria <justincambria@Justins-MacBook-Air.local>
2023-04-25 14:47:46 -0400

    more changes for the better. Meeting with client tomorrow

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { BiDumbbell } from "react-icons/bi";
-
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="welcome" id="home">
       <h1>
-          Columbus Sports Connection <br />
+        Columbus Sports Connection <br />
         <p className="p__text"> A Total Fitness Facility.</p>
-        <p className="p__intro">Columbus Sports Connection, nestled in Clintonville, 
-        is a premier workout facility with 20 years of experience. As a locally owned health club, 
-        we offer a welcoming atmosphere for fitness newbies and seasoned enthusiasts alike. 
-        
-        Join us at Columbus Sports Connection to start your fitness journey with confidence!
-          </p>
-          <button>Get Started</button>
+        <p className="p__intro">
+          Columbus Sports Connection, nestled in Clintonville, is a premier workout facility with 20 years of experience. As a locally owned health club, we offer a welcoming atmosphere for fitness newbies and seasoned enthusiasts alike. Join us at Columbus Sports Connection to start your fitness journey with confidence!
+        </p>
+        <button className="header__button">Get Started</button>
       </h1>
       <div></div>
     </div>
